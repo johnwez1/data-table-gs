133eee0d3cbff3d668da4c0134e906efeeac2e94
johnwez1 <j.f.weatherall@gmail.com>
2018-06-30 12:01:02 +1000

    parse conditions working

--- parseconditions.js
+++ parseconditions.js
@@ -1,33 +1,66 @@
 function testArray() {
-    var x = ['arr1', '1', '2', '3'];
-    var y = ['arr2', 'A', 'B', 'C'];
-    var z = ['arr3', 'x', 'y', 'z'];
-    var data = ['data', 'DATA', 'DATA', 'DATA']
+    var x = ['arr1', '1', '2', '3', '4', '5', '6'];
+    var y = ['arr2', 'a', 'b', 'c', 'A', 'B', 'C'];
+    var z = ['arr3', 'x', 'y', 'z', 'X', 'Y', 'Z'];
+    var data = ['data', 'DATA', 'DATA', 'DATA', 'DATA', 'DATA', 'DATA']
     
     return [x, y, z, data]
 }
 
+function detailedArray() {
+    var x = ['arr1', '1', '2', '3', '4', '5', '6'];
+    var y = ['arr2', 'a', 'b', 'c', 'A', 'B', 'C'];
+    var z = ['arr3', 'x', 'y', 'z', 'X', 'Y', 'Z'];
+    var data = ['data', 'DATA123456', 'DATA12456', 'DATA123', 'DATA123', 'DATA123', 'DATA456']
+    
+    return [x, y, z, data]
+}
 
 function evaluateExpression(cond, arr) {
-    var cond = 'arr1 is 2 AND (arr2 is not A OR arr3 contains z)'
-    cond = cond.replace(' AND ', '*').replace(' OR ', '+')
+  //testing only
+//    var arr = detailedArray()
+//    var cond = '(((data contains 123 OR arr3 contains Z OR data contains 3456)) AND arr1 contains not A)'// AND (arr2 is a OR arr2 is B)'
+    cond = '(' + cond + ')'
+    
+    var newCond = cond.replace(/ AND /, '*').replace(/ OR /, '+')
+    while(newCond != cond){
+      newCond = newCond.replace(/ AND /, '*').replace(/ OR /, '+')
+      cond = cond.replace(/ AND /, '*').replace(/ OR /, '+')
+    }
+    
+    cond = cond.split('')
+    Logger.log(cond)
     //also take out double whitespace
     var el
+    var index
     //pad with ( + expr + )
     expressionStack = ['']
     operatorStack = []
     for(c in cond){
-        el = cond[c]
-        var prevOperator = operatorStack[operatorStack.length - 1]
-        var prevExpression = expressionStack[expressionStack.length - 2]  
-        var curExpression = expressionStack[expressionStack.length - 1]
-        console.log(prevOperator, prevExpression, curExpression, el) 
+        index = parseInt(c)
+        el = cond[index]
+        
         if(['(', ')', '*', '+'].indexOf(el) > -1){
-            operatorStack.push(el)
             
-//            if(el == '('){
-//                continue
-//            }
+            operatorStack.push(el)
+            var prevOperator = operatorStack[operatorStack.length - 2]
+            var prevExpression = expressionStack[expressionStack.length - 2]  
+            var curExpression = expressionStack[expressionStack.length - 1]
+            
+            
+            Logger.log(operatorStack)
+            Logger.log(expressionStack)
+            Logger.log(el)
+            Logger.log(prevOperator)
+            
+            if(el == ')' && prevOperator == '('){
+               //()
+               expressionStack[expressionStack.length - 1] = 
+                 evaluateBasicExpression(curExpression, arr)
+               operatorStack.pop()
+               operatorStack.pop()
+               continue
+            }
             
             if(el == '*' || el == '+' || el == ')'){
                 if(prevOperator == '+' || prevOperator == '*'){
@@ -36,6 +69,7 @@ function evaluateExpression(cond, arr) {
                     //statement finished, evaluate base and push up level
                     expressionStack[expressionStack.length - 1] = 
                         evaluateBasicExpression(curExpression, arr)
+                    
                     //combine base with row above
                     expressionStack[expressionStack.length - 2] = 
                     compoundBooleanArr(expressionStack[expressionStack.length - 1],
@@ -53,26 +87,36 @@ function evaluateExpression(cond, arr) {
                     if (el == '*' || el == '+'){
                         operatorStack.pop()
                         operatorStack.pop()
+                        operatorStack.push(el)
                     }
-                } else if (prevOperator != '('){
-                    //statement finished, evaluate base and push up level
+                }
+              
+                if (prevOperator == '(' || prevOperator == null){
+                    //statement finished, evaluate
                     expressionStack[expressionStack.length - 1] = 
                         evaluateBasicExpression(curExpression, arr)
                 }
-                expressionStack.push('') //we push if we know the next char will not be symbol
             }
             
-            if(el == ')' && prevOperator == '('){
-               //()
-               operatorStack.pop()
-               operatorStack.pop()
+            if(['(', '*', '+', ')'].indexOf(cond[index+1]) == -1){
+              //only push if you know next thing is not a symbol and it's not the last char, dont push first in this variation
+              if(index != 0 && index < cond.length - 1){
+                expressionStack.push('')
+              } else {
+//                Logger.log("last element up next, no push")
+//                Logger.log(cond[index+1])
+              }
             }
+            
         } else {
             //partial push to string only
             expressionStack[expressionStack.length - 1] = expressionStack[expressionStack.length - 1] +  el
         }
-        console.log(expressionStack, operatorStack, el)      
+        //Logger.log(expressionStack)
+//        Logger.log(operatorStack)
+        //Logger.log(el)      
     }
+    //confirm the structure we have here
     return expressionStack
 }
 
@@ -84,32 +128,36 @@ function isBasicExpression(statement) {
     }
 }
 
-function __evaluateBasicExpression(statement, arr) {
-    var parsedStatement = parseBasicExpression(statement)
-    var lh_index = namedFieldIndex(parsedStatement["lh"], arr)
-    switch (parsedStatement["comparator"]) {
-        case ' is not ':
-            return arr[lh_index].map(function(val) {return val != parsedStatement["rh"]});
-            break;
-        case ' is ':
-            return arr[lh_index].map(function(val) {return val == parsedStatement["rh"]});
-            break;
-        case ' contains ':
-            return arr[lh_index].map(function(val) {return (val.indexOf(parsedStatement["rh"]) > -1)});
-            break;
-        case ' contains not ':
-            return arr[lh_index].map(function(val) {return (val.indexOf(parsedStatement["rh"]) == -1)});
-            break;
-        case ' after ':
-            return arr[lh_index].map(function(val) {return (val > parsedStatement["rh"])});
-            break;
-        case ' before ':
-            return arr[lh_index].map(function(val) {return (val < parsedStatement["rh"])});
-            break;
-    }
+function evaluateBasicExpression(statement, arr) {
+  if(isArray(statement)){
+    //check if it has already been evaluated
+    return(statement)
+  }
+  var parsedStatement = parseBasicExpression(statement)
+  var lh_index = namedFieldIndex(parsedStatement["lh"], arr)
+  switch (parsedStatement["comparator"]) {
+    case ' is not ':
+      return arr[lh_index].map(function(val) {return val != parsedStatement["rh"]});
+      break;
+    case ' is ':
+      return arr[lh_index].map(function(val) {return val == parsedStatement["rh"]});
+      break;
+    case ' contains ':
+      return arr[lh_index].map(function(val) {return (val.indexOf(parsedStatement["rh"]) > -1)});
+      break;
+    case ' contains not ':
+      return arr[lh_index].map(function(val) {return (val.indexOf(parsedStatement["rh"]) == -1)});
+      break;
+    case ' after ':
+      return arr[lh_index].map(function(val) {return (val > parsedStatement["rh"])});
+      break;
+    case ' before ':
+      return arr[lh_index].map(function(val) {return (val < parsedStatement["rh"])});
+      break;
+  }
 }
 
-function evaluateBasicExpression(statement) {
+function __evaluateBasicExpression(statement) { //simple version for testing only
     var parsedStatement = parseBasicExpression(statement)
     //var lh_index = namedFieldIndex(parsedStatement["lh"], arr)
     switch (parsedStatement["comparator"]) {
@@ -171,12 +219,55 @@ function transpose(a)
   return Object.keys(a[0]).map(function (c) { return a.map(function (r) { return r[c]; }); });
 }
 
-function compoundBooleanArr(arr1, arr2, operator){
-    console.log('compounBooleanArr', arr1, arr2, operator)
+function compoundBooleanArr(arr1, arr2, operator){    
+    //need to use map because && || operators dont work on arrays as expected, 
+    //eg x = [true, false, false], y = [true, true, false]
+    // but (y || x) != (x || y) 
     if(operator == '*'){
-        return arr1 && arr2
+      return arr1.map(function(val, i) {return (val && arr2[i])})
     }
     if(operator == '+'){
-        return arr1 || arr2
+      return arr1.map(function(val, i) {return (val || arr2[i])})
     }   
+}
+
+function checkConditions(cond){
+  //check if any remaining parenthesis anywhere
+  if (cond.match(/\(/g).length != cond.match(/\)/g).length){
+    return false
+  }
+  
+  //reduce pairings of parenthesis
+  while (cond.match(/\(([^()]+)\)/g)){
+    //check innermost
+    var innermost = cond.match(/\(([^()]+)\)/g);
+    for (match in innermost){
+      if(!(parseBoolStatement(innermost[match]))){
+        return false
+      }
+    }
+    
+    cond = cond.replace(/\(([^()]+)\)/g, 'valid is valid')    
+    Logger.log('list of conditions becomes:')
+    Logger.log(cond)
+  }
+  
+  //check if any remaining parenthesis anywhere
+  
+  if ((cond.match(/\(/g) || cond.match(/\)/g))){
+    return false
+  }
+  return true
+}
+
+function parseSplit(expression){
+  //expression must be parseable as JSON!!!
+  //var expression = '{"field":"FIELDNAME", "by":"SPLITAROUND", "index":INT (KEEP THIS INDEX)}' //if 
+  var expressionJSON = JSON.parse(expression)
+  if(expressionJSON.field == null || expressionJSON.by == null || expressionJSON.index == null){return} 
+  return expressionJSON
+}
+
+function isArray(x) {
+    return x.constructor.toString().indexOf("Array") > -1;
 }
